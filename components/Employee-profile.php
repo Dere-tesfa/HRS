f744f7ff677d3a8dba7ab20901796acc792a3af2
Tadios <tadiosmisganaw@gmail.com>
2025-12-16 16:40:15 +0300

    style the employee profile

---
+++ components/Employee-profile.php
@@ -0,0 +1,55 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>employee profile</title>
+  <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../font/css/all.css">
+    <link rel="stylesheet" href="../font/css/all.min.css">
+     <script src="../font/js/all.js"></script>
+    <script src="../font/js/all.min.js"></script>
+</head>
+<body></body>
+  <header>
+  <div class="header_content">
+                <div class="header-logo"><a href="../Html/index.php"><img src="../images/logo.png" alt="logo"></a>
+                </div>
+                <h2>HR-System</h2>
+            </div>  
+          </header>
+  <div class="employee_page_container">
+    <aside class="employee_dashboard">
+              <ul class="employee_main_page">
+                  <li > <i class="fa-solid fa-city dashboard_icons
+                          "></i><a class="dashboard_links
+                          " href="Employee-dashboard.php">Dashboard</a></li>
+                 </li>
+                  <li class="active"> <i class="fa-solid fa-user dashboard_icons
+                          "></i><a class="dashboard_links
+                          " href="./Employee-profile.php">Profile</a></li>
+                  <li> <i class="fa-solid fa-clipboard-user dashboard_icons
+                          "></i><a class="dashboard_links
+                          " href="#">Attendance</a></li>
+                  <li> <i class="fa-brands fa-paypal dashboard_icons
+                          "></i><a class="dashboard_links
+                          " href="#">Payroll</a></li>
+                  <li> <i class="fa-solid fa-ban dashboard_icons
+                          "></i><a class="dashboard_links
+                          " href="#">Leave</a></li>
+              </ul>
+          </aside>
+          <div class="employee_profile_container">
+          <h2>Employee Profile Page</h2>
+          <p>Welcome to your profile page. Here you can view and update your personal information.</p>
+    <div class="profile_card">
+ <i class="fa-solid fa-user profile_icon"></i><h3>Tadios misganaw</h3>
+    <p><strong>Position:</strong> Software Engineer</p>
+    <p><strong>Email:</strong> tadi@grok.com</p>
+    <p><strong>Phone:</strong> (123) 456-7890</p>
+    <button class="update_button"><a href="signup.php">Update Profile</a></button>
+    </div>
+          </div>
+    </html>
+    </body>
+  </div>
+</html>
